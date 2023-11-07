871bb06f132601100ae67a79e1f518d97fb63270
logith vikram <106614043+logithvikram@users.noreply.github.com>
2023-11-07 18:19:37 +0530

    Update MovieRepository.java

--- src/main/java/com/example/movie/repository/MovieRepository.java
+++ src/main/java/com/example/movie/repository/MovieRepository.java
@@ -1,2 +1,13 @@
-package com.example.movie.repository;public interface MovieRepository {
+package com.example.movie.repository;
+
+import com.example.movie.entity.Movie;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+    Optional<Movie> findMovieByImdbId(String imdbId);
 }
